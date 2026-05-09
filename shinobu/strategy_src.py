bd42a2da6fbe37093383f92ea120a3f3a8c87186
parksuseong <godsuseong@gmail.com>
2026-05-09 18:07:35 +0900

    Tune blog_scr profile to Jan-Apr optimized parameters

--- shinobu/strategy_src.py
+++ shinobu/strategy_src.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 
 STOCH_PERIOD = 5
-CCI_PERIOD = 20
-RSI_PERIOD = 14
+CCI_PERIOD = 14
+RSI_PERIOD = 5
 
 
 @dataclass(frozen=True)
@@ -75,15 +75,15 @@ SRC_PROFILE_OPTIONS = {
     "normal": NORMAL_5M_PROFILE,
     "active": ACTIVE_5M_PROFILE,
     "blog_scr": SrcProfile(
-        stoch_oversold=20.0,
-        stoch_overbought=80.0,
-        cci_oversold=-100.0,
-        cci_overbought=100.0,
-        rsi_oversold=35.0,
-        rsi_overbought=70.0,
+        stoch_oversold=15.0,
+        stoch_overbought=75.0,
+        cci_oversold=-120.0,
+        cci_overbought=110.0,
+        rsi_oversold=40.0,
+        rsi_overbought=60.0,
         open_prev_need=3,
         open_cross_need=2,
-        close_need=3,
+        close_need=2,
         label="SCR Blog",
     ),
 }
